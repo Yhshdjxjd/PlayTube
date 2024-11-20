d9db16cb18436582a40760caba7d2094af269748
PlayTube <prankking592@gmail.com>
2024-11-20 10:58:44 +0600

    Create signup.php
    All details

---
+++ signup.php
@@ -0,0 +1,23 @@
+PHP
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    // ডেটাবেজ সংযোগ
+    $conn = new mysqli('localhost', 'root', '', 'playtube');
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+    if ($conn->query($sql) === TRUE) {
+        echo "Signup successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+}
+?>
